b99d0c5a6d98b63f880a6d95ea625de8856a17cd
Drew Walters <drewwalt@us.ibm.com>
2016-02-08 15:57:41 -0600

    Delay urth-core-bind render till dependencies loaded.
    (c) Copyright IBM Corp. 2016

--- nb-extension/js/main.js
+++ nb-extension/js/main.js
@@ -6,6 +6,16 @@ define([
     './init/init'
 ], function(init) {
     'use strict';
+
+    // Some versions of IE do not have window.console defined. Some versions
+    // do not define the debug and other methods. This is a minimal workaround
+    // based on what declarative widgets code is using.
+    window.console = window.console || {};
+    window.console.log = window.console.log || function() {};
+    ['debug', 'error', 'trace', 'warn'].forEach(function(method) {
+        window.console[method] = window.console[method] || window.console.log;
+    });
+    
     init(IPython ? IPython.notebook.base_url : '/');
     return {
       load_ipython_extension: function() { console.debug('Custom JS loaded'); }
