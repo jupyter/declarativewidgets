06312ec5a8297c69f86fca79c92c116e81c044b3
Drew Walters <drewwalt@us.ibm.com>
2016-04-01 11:12:18 -0500

    Fix test failures on IE.
    (c) Copyright IBM Corp. 2016

--- wct.conf.js
+++ wct.conf.js
@@ -21,7 +21,18 @@ module.exports = {
           {
               browserName: "chrome",
               platform: "Windows 10"
+          },
+          {
+              browserName: "internet explorer",
+              platform: "Windows 8.1"
           }
+        // Don't run MicrosoftEdge until Windows 10 Anniversary update
+        // is available as the nested template test fails
+        // in urth-core-bind before that update.
+        //   {
+        //       browserName: "MicrosoftEdge",
+        //       platform: "Windows 10"
+        //   }
       ]
     }
   },
