3aff26cec7ea69506c17649c9f6979cfbce15a09
Gino Bustelo <lbustelo@us.ibm.com>
2016-06-08 11:19:13 -0500

    [skip ci] Bump to 0.5.4.dev (c) Copyright IBM Corp. 2016

--- nb-extension/python/urth/widgets/ext/_version.py
+++ nb-extension/python/urth/widgets/ext/_version.py
@@ -1,5 +1,5 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 5, 3)
+version_info = (0, 5, 4, 'dev')
 __version__ = '.'.join(map(str, version_info))
