1f2cb177f01e41d84bd15bd627019cfb6a268b3c
Gino Bustelo <lbustelo@us.ibm.com>
2016-07-22 13:31:13 -0500

    Fix issues with loading ipywidgets 4.1.x dependencies in jupyter 4.2 (c) Copyright IBM Corp. 2016

--- nb-extension/js/main.js
+++ nb-extension/js/main.js
@@ -17,37 +17,40 @@ define([
 
     return {
         load_ipython_extension: function () {
-            //create a mapping for declarativewidgets and its dependencies
-            requirejs.config({
-                map: {
-                    '*': {
-                        'jupyter-decl-widgets': 'nbextensions/declarativewidgets/js/widgets'
+            return new Promise(function(resolve) {
+                //create a mapping for declarativewidgets and its dependencies
+                requirejs.config({
+                    map: {
+                        '*': {
+                            'jupyter-decl-widgets': 'nbextensions/declarativewidgets/js/widgets'
+                        },
+                        'nbextensions/declarativewidgets/js': {
+                            'jupyter-js-widgets': 'ipywidgets4-or-jupyter-js-widgets'
+                        }
                     },
-                    'nbextensions/declarativewidgets/js': {
-                        'jupyter-js-widgets': 'ipywidgets4-or-jupyter-js-widgets'
+                    paths: {
+                        'ipywidgets4-or-jupyter-js-widgets': [
+                            'does/not/exist',  //HACK: fallbacks was acting strange with 2 items
+                            Jupyter.notebook.base_url+'nbextensions/widgets/widgets/js/widget',
+                            Jupyter.notebook.base_url+'nbextensions/jupyter-js-widgets/extension',
+                        ]
                     }
-                },
-                paths: {
-                    'ipywidgets4-or-jupyter-js-widgets': [
-                        'does/not/exist',  //HACK: fallbacks was acting strange with 2 items
-                        Jupyter.notebook.base_url+'nbextensions/widgets/widgets/js/widget',
-                        Jupyter.notebook.base_url+'nbextensions/jupyter-js-widgets/extension',
-                    ]
-                }
-            });
-
-            require([
-                'base/js/namespace',
-                'base/js/events',
-                './init/init'
-            ],function(Jupyter, events, init) {
-                init({
-                    namespace: Jupyter,
-                    events: events
                 });
 
-                load_css(window.Urth._getModuleBasedComponentRoot() + 'css/main.css');
-                console.debug('loaded declarativewidgets');
+                require([
+                    'base/js/namespace',
+                    'base/js/events',
+                    './init/init'
+                ],function(Jupyter, events, init) {
+                    init({
+                        namespace: Jupyter,
+                        events: events
+                    });
+
+                    load_css(window.Urth._getModuleBasedComponentRoot() + 'css/main.css');
+                    console.debug('loaded declarativewidgets');
+                    resolve();
+                });
             });
         }
     };
