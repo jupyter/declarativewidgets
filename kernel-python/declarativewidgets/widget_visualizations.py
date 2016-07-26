4476e163610bd9983575afbeb7d8a22d5c1e78f9
Drew Logsdon <dalogsdon@gmail.com>
2016-07-26 18:56:33 -0500

    [Issue 465] Added template with channel and selection var to python explore function
    (c) Copyright IBM Corp. 2016

--- kernel-python/declarativewidgets/widget_visualizations.py
+++ kernel-python/declarativewidgets/widget_visualizations.py
@@ -3,8 +3,12 @@
 
 from IPython.core.display import display, HTML
 
-def explore(df):
+def explore(df, channel='default', selection_var=None):
     """Renders the urth-viz-explorer widget to the user output"""
-    display(HTML("<link rel='import' href='urth_components/declarativewidgets-explorer/urth-viz-explorer.html' \
-                    is='urth-core-import' package='jupyter-incubator/declarativewidgets_explorer'> \
-                    <urth-viz-explorer ref='{}'></urth-viz-explorer>".format(df)))
+    display(HTML(
+        """<link rel='import' href='urth_components/declarativewidgets-explorer/urth-viz-explorer.html'
+               is='urth-core-import' package='jupyter-incubator/declarativewidgets_explorer'>
+           <template is="urth-core-bind" channel="{channel}">
+               <urth-viz-explorer ref='{ref}' {selection}></urth-viz-explorer>
+           </template>""".format(ref=df, channel=channel, selection = 'selection="{{{{{}}}}}"'.format(selection_var) if selection_var else '')
+    ))
