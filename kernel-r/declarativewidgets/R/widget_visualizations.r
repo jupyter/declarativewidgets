d9b6e73d186b32e66b9f634f3490739e27670c60
Michael Poplavski <mikepop77@gmail.com>
2016-07-27 13:02:39 -0500

    Adds explore function channel and seleciton template to r and scala
    (c) Copyright IBM Corp. 2016

--- kernel-r/declarativewidgets/R/widget_visualizations.r
+++ kernel-r/declarativewidgets/R/widget_visualizations.r
@@ -1,7 +1,10 @@
 #Widget Visualization one line displays
 
-explore <- function(df) {
+explore <- function(df, channel='default', selection_var=NULL) {
+    selection <- ifelse(is.null(selection_var), "", paste("selection={{", selection_var, "}}", sep=""))
     IRdisplay::display_html(paste("<link rel='import' href='urth_components/declarativewidgets-explorer/urth-viz-explorer.html'
                                     is='urth-core-import' package='jupyter-incubator/declarativewidgets_explorer'>
-                                    <urth-viz-explorer ref='", df, "'></urth-viz-explorer>", sep = ""))
+                                    <template is='urth-core-bind' channel='", channel, "'>
+                                        <urth-viz-explorer ref='", df, "' ", selection, "></urth-viz-explorer>",
+                                    "</template>", sep = ""))
 }
--- kernel-scala/src/main/scala/declarativewidgets/WidgetVisualizations.scala
+++ kernel-scala/src/main/scala/declarativewidgets/WidgetVisualizations.scala
@@ -14,12 +14,16 @@ object WidgetVisualizations {
     *
     * @param df Dataframe variable name to render/explore
     */
-  def explore(df: String) = {
+  def explore(df: String, channel: String = "default", selectionVar: String = null) = {
+    val selection = if(selectionVar == null) "" else s"selection={{$selectionVar}}"
     val explorerImport =
       """
         <link rel='import' href='urth_components/declarativewidgets-explorer/urth-viz-explorer.html'
           is='urth-core-import' package='jupyter-incubator/declarativewidgets_explorer'>
       """
-    getKernel.display.html(s"$explorerImport <urth-viz-explorer ref='$df'></urth-viz-explorer>")
+    getKernel.display.html(s"$explorerImport " +
+                           s"<template is='urth-core-bind' channel='$channel'>" +
+                              s"<urth-viz-explorer ref='$df' $selection></urth-viz-explorer>" +
+                            "</template>")
   }
 }
