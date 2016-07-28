148b64e52d5b80fc48d08135c8d239326e7b35b8
Michael Poplavski <mikepop77@gmail.com>
2016-07-28 15:47:46 -0500

    Explore function can take in actual df or string of df variable
    (c) Copyright IBM Corp. 2016

--- kernel-python/declarativewidgets/widget_visualizations.py
+++ kernel-python/declarativewidgets/widget_visualizations.py
@@ -2,13 +2,32 @@
 # Distributed under the terms of the Modified BSD License.
 
 from IPython.core.display import display, HTML
+import pandas
 
 def explore(df, channel='default', selection_var=None):
-    """Renders the urth-viz-explorer widget to the user output"""
+    """
+    Renders the urth-viz-explorer widget to the user output
+    If pandas.DataFrame assign with unique name to user namespace else use what was passed in the string
+
+    Parameters
+    ----------
+    df              The dataframe itself or the string representation of the variable
+    channel         The channel to bind to defaulted to default
+    selection_var   The selection variable by default not used/applied
+    """
+
+    explore_df = "unique_explore_df_name"
+    if isinstance(df, pandas.DataFrame):
+        get_ipython().user_ns[explore_df] = df
+    else:
+        explore_df = df
+
+    selection = 'selection="{{{{{}}}}}"'.format(selection_var) if selection_var else ''
+
     display(HTML(
         """<link rel='import' href='urth_components/declarativewidgets-explorer/urth-viz-explorer.html'
                is='urth-core-import' package='jupyter-incubator/declarativewidgets_explorer'>
            <template is="urth-core-bind" channel="{channel}">
                <urth-viz-explorer ref='{ref}' {selection}></urth-viz-explorer>
-           </template>""".format(ref=df, channel=channel, selection = 'selection="{{{{{}}}}}"'.format(selection_var) if selection_var else '')
+           </template>""".format(ref=explore_df, channel=channel, selection=selection)
     ))
--- kernel-r/declarativewidgets/R/widget_visualizations.r
+++ kernel-r/declarativewidgets/R/widget_visualizations.r
@@ -1,10 +1,28 @@
-#Widget Visualization one line displays
+# Widget Visualization one line displays
 
+#' Explore function
+#'
+#' The explore function is a wrapper around urth-viz-explorer.
+#' See the jupyter-incubator/declarativewidgets_explorer project.
+#'
+#' @name explore
+#' @param df  the dataframe itself or the string representation of the variable
+#' @param channel  The channel to bind to defaulted to default
+#' @param selection_var  The selection variable by default not used/applied
 explore <- function(df, channel='default', selection_var=NULL) {
+    unique_df_name <- "the_literal_template_df_name"
+    register_explore_df <- function(df) {
+        #assigns df to global env and returns a unique name
+        assign(unique_df_name, df, envir = .GlobalEnv)
+        return (unique_df_name)
+    }
+
+    exlore_df_name <- ifelse(class(df) == "data.frame", register_explore_df(df), df)
     selection <- ifelse(is.null(selection_var), "", paste("selection={{", selection_var, "}}", sep=""))
+
     IRdisplay::display_html(paste("<link rel='import' href='urth_components/declarativewidgets-explorer/urth-viz-explorer.html'
                                     is='urth-core-import' package='jupyter-incubator/declarativewidgets_explorer'>
                                     <template is='urth-core-bind' channel='", channel, "'>
-                                        <urth-viz-explorer ref='", df, "' ", selection, "></urth-viz-explorer>",
+                                        <urth-viz-explorer ref='", exlore_df_name, "' ", selection, "></urth-viz-explorer>",
                                     "</template>", sep = ""))
 }
